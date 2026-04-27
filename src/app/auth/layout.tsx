2904a0652430e4cb418abcb077627a606d93af79
yudopri <e41230009@student.polije.ac.id>
2026-04-27 08:51:07 +0700

    fix Aset build

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
